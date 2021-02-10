21ac4cfdb316c495f6612db8812de27699cc14b6
housemax77 <63367971+housemax77@users.noreply.github.com>
2021-02-09 20:13:24 -0600

    Removed useless code

--- index.js
+++ index.js
@@ -104,44 +104,6 @@ function addLi(element, index) {
 }
 
 function callSortAlpabetical(event) {
-  // var list,
-  //   i,
-  //   switching,
-  //   b,
-  //   shouldSwitch,
-  //   dir,
-  //   switchcount = 0;
-  // list = document.getElementById("List");
-  // switching = true;
-  // dir = "asc";
-  // while (switching) {
-  //   switching = false;
-  //   b = list.getElementsByTagName("LI");
-  //   for (i = 0; i < b.length - 1; i++) {
-  //     shouldSwitch = false;
-  //     if (dir == "asc") {
-  //       if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
-  //         shouldSwitch = true;
-  //         break;
-  //       }
-  //     } else if (dir == "desc") {
-  //       if (b[i].innerHTML.toLowerCase() < b[i + 1].innerHTML.toLowerCase()) {
-  //         shouldSwitch = true;
-  //         break;
-  //       }
-  //     }
-  //   }
-  //   if (shouldSwitch) {
-  //     b[i].parentNode.insertBefore(b[i + 1], b[i]);
-  //     switching = true;
-  //     switchcount++;
-  //   } else {
-  //     if (switchcount == 0 && dir == "asc") {
-  //       dir = "desc";
-  //       switching = true;
-  //     }
-  //   }
-  // }
   document.getElementById("sortBy").innerHTML = "Sorting Alphabetically";
   localStorage.setItem("sortBy", "Sorting Alphabetically");
 
